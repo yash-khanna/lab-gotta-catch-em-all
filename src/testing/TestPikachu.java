9e227b8541253cf0f212142870774abf74d583a7
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-05-01 18:13:59 +0530

    Update TestPikachu.java

--- src/testing/TestPikachu.java
+++ src/testing/TestPikachu.java
@@ -1,70 +1,71 @@
-//package testing;
-//
-//import org.junit.Test;
-//
-//import model.Pikachu;
-//
-//import static org.junit.Assert.*;
-//
-//public class TestPikachu {
-//	
-//	@Test
-//	public void testCharacteristicsMethod() {
-//		Pikachu p = new Pikachu("AAA",4);
-//		String temp = "Pikachu is an Electric-type Pok�mon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pok�mon franchise mascot.";
-//		assertEquals(temp,p.characteristics());		
-//		try {
-//			Pikachu p1 = new Pikachu(null,0);
-//			temp = null;
-//			p1.characteristics();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testTypeMethod() {
-//		Pikachu p = new Pikachu("AAA",4);
-//		String temp = "ELECTRIC";
-//		assertEquals(temp,p.type());		
-//		try {
-//			Pikachu p1 = new Pikachu(null,0);
-//			temp = null;
-//			p1.type();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testEvolutionMethod() {
-//		Pikachu p = new Pikachu("AAA",4);
-//		String temp = "  RAICHU";
-//		assertEquals(temp,p.evolution());		
-//		try {
-//			Pikachu p1 = new Pikachu(null,0);
-//			temp = null;
-//			p1.evolution();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testBaseExpMethod() {
-//		Pikachu p = new Pikachu("AAA",4);
-//		String temp = "112";
-//		assertEquals(temp,p.baseExp());		
-//		try {
-//			Pikachu p1 = new Pikachu(null,0);
-//			temp = null;
-//			p1.baseExp();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package testing;
+/*
+import org.junit.Test;
+
+import model.Pikachu;
+
+import static org.junit.Assert.*;
+
+public class TestPikachu {
+	
+	@Test
+	public void testCharacteristicsMethod() {
+		Pikachu p = new Pikachu("AAA",4,"Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.","ELECTRIC","  RAICHU",112);
+		String temp = "Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.";
+		assertEquals(temp,p.getCharacteristics());		
+		try {
+			Pikachu p1 = new Pikachu(null,0,null,null,null,0);
+			temp = null;
+			p1.getCharacteristics();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testTypeMethod() {
+		Pikachu p = new Pikachu("AAA",4,"Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.","ELECTRIC","  RAICHU",112);
+		String temp = "ELECTRIC";
+		assertEquals(temp,p.getType());		
+		try {
+			Pikachu p1 = new Pikachu(null,0,null,null,null,0);
+			temp = null;
+			p1.getType();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testEvolutionMethod() {
+		Pikachu p = new Pikachu("AAA",4,"Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.","ELECTRIC","  RAICHU",112);
+		String temp = "  RAICHU";
+		assertEquals(temp,p.getEvolution());		
+		try {
+			Pikachu p1 = new Pikachu(null,0,null,null,null,0);
+			temp = null;
+			p1.getEvolution();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testBaseExpMethod() {
+		Pikachu p = new Pikachu("AAA",4,"Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.","ELECTRIC","  RAICHU",112);
+		int temp = 112;
+		assertEquals(temp,p.getBaseExp());		
+		try {
+			Pikachu p1 = new Pikachu(null,0,null,null,null,0);
+			temp = 0;
+			p1.getBaseExp();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
