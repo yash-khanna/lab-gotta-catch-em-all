3dc0dd29b8f08c94f9d67a93942a61a1254afdef
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-05-01 18:11:02 +0530

    Update TestCharizard.java

--- src/testing/TestCharizard.java
+++ src/testing/TestCharizard.java
@@ -1,69 +1,71 @@
-//package testing;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.Test;
-//
-//import model.Charizard;
-//
-//public class TestCharizard {
-//	@Test
-//	public void testCharacteristicsMethod() {
-//		Charizard p = new Charizard("AAA",4);
-//		String temp = "Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.";
-//		assertEquals(temp,p.characteristics());		
-//		try {
-//			Charizard p1 = new Charizard(null,0);
-//			temp = null;
-//			p1.characteristics();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testTypeMethod() {
-//		Charizard p = new Charizard("AAA",4);
-//		String temp = "FIRE";
-//		assertEquals(temp,p.type());		
-//		try {
-//			Charizard p1 = new Charizard(null,0);
-//			temp = null;
-//			p1.type();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testEvolutionMethod() {
-//		Charizard p = new Charizard("AAA",4);
-//		String temp = "CHARMELEON";
-//		assertEquals(temp,p.evolution());		
-//		try {
-//			Charizard p1 = new Charizard(null,0);
-//			temp = null;
-//			p1.evolution();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testBaseExpMethod() {
-//		Charizard p = new Charizard("AAA",4);
-//		String temp = "240";
-//		assertEquals(temp,p.baseExp());		
-//		try {
-//			Charizard p1 = new Charizard(null,0);
-//			temp = null;
-//			p1.baseExp();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package testing;
+
+/*
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import model.Bulbasaur;
+import model.Charizard;
+
+public class TestCharizard {
+	@Test
+	public void testCharacteristicsMethod() {
+		Charizard p = new Charizard("AAA",4,"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.","FIRE","CHARMELEON",240);
+		String temp = "Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.";
+		assertEquals(temp,p.getCharacteristics());		
+		try {
+			Charizard p = new Charizard(null,0,null,null,null,0);
+			temp = null;
+			bulb.getCharacteristics();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testTypeMethod() {
+		Charizard p = new Charizard("AAA",4,"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.","FIRE","CHARMELEON",240);
+		String temp = "FIRE";
+		assertEquals(temp,p.getType());		
+		try {
+			Charizard p = new Charizard(null,0,null,null,null,0);
+			temp = null;
+			p1.getType();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testEvolutionMethod() {
+		Charizard p = new Charizard("AAA",4,"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.","FIRE","CHARMELEON",240);
+		String temp = "CHARMELEON";
+		assertEquals(temp,p.getEvolution());		
+		try {
+			Charizard p = new Charizard(null,0,null,null,null,0);
+			temp = null;
+			p1.getEvolution();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testBaseExpMethod() {
+		Charizard p = new Charizard("AAA",4,"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.","FIRE","CHARMELEON",240);
+		int temp = 240;
+		assertEquals(temp,p.getBaseExp());		
+		try {
+			Charizard p = new Charizard(null,0,null,null,null,0);
+			temp = 0;
+			p1.getBaseExp();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
