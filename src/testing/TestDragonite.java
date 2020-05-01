85a61d800c3f644083ffdeb2d4f97ca7d611c926
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-05-01 18:12:18 +0530

    Update TestDragonite.java

--- src/testing/TestDragonite.java
+++ src/testing/TestDragonite.java
@@ -1,69 +1,71 @@
-//package testing;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.Test;
-//
-//import model.Dragonite;
-//
-//public class TestDragonite {
-//	@Test
-//	public void testCharacteristicsMethod() {
-//		Dragonite p = new Dragonite("AAA",4);
-//		String temp = "Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pok�mon with human-like intelligence. It shows signs of altruism.";
-//		assertEquals(temp,p.characteristics());		
-//		try {
-//			Dragonite p1 = new Dragonite(null,0);
-//			temp = null;
-//			p1.characteristics();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testTypeMethod() {
-//		Dragonite p = new Dragonite("AAA",4);
-//		String temp = "DRAGON";
-//		assertEquals(temp,p.type());		
-//		try {
-//			Dragonite p1 = new Dragonite(null,0);
-//			temp = null;
-//			p1.type();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testEvolutionMethod() {
-//		Dragonite p = new Dragonite("AAA",4);
-//		String temp = "DRATINI";
-//		assertEquals(temp,p.evolution());		
-//		try {
-//			Dragonite p1 = new Dragonite(null,0);
-//			temp = null;
-//			p1.evolution();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testBaseExpMethod() {
-//		Dragonite p = new Dragonite("AAA",4);
-//		String temp = "270";
-//		assertEquals(temp,p.baseExp());		
-//		try {
-//			Dragonite p1 = new Dragonite(null,0);
-//			temp = null;
-//			p1.baseExp();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package testing;
+/*
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import model.Charizard;
+import model.Dragonite;
+
+public class TestDragonite {
+	@Test
+	public void testCharacteristicsMethod() {
+		Dragonite p = new Dragonite("AAA",4,"Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.","DRAGON","DRATINI",270);
+		String temp = "Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.";
+		assertEquals(temp,p.getCharacteristics());		
+		try {
+			Dragonite p = new Dragonite(null,0,null,null,null,0);
+			temp = null;
+			p1.getCharacteristics();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testTypeMethod() {
+		Dragonite p = new Dragonite("AAA",4,"Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.","DRAGON","DRATINI",270);
+		String temp = "DRAGON";
+		assertEquals(temp,p.getType());		
+		try {
+			Dragonite p = new Dragonite(null,0,null,null,null,0);
+			temp = null;
+			p1.getType();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testEvolutionMethod() {
+		Dragonite p = new Dragonite("AAA",4,"Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.","DRAGON","DRATINI",270);
+		String temp = "DRATINI";
+		assertEquals(temp,p.getEvolution());		
+		try {
+			Dragonite p = new Dragonite(null,0,null,null,null,0);
+			temp = null;
+			p1.getEvolution();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testBaseExpMethod() {
+		Dragonite p = new Dragonite("AAA",4,"Dragonite is capable of flying faster than the speed of sound. It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.","DRAGON","DRATINI",270);
+		int temp = 270;
+		assertEquals(temp,p.getBaseExp());		
+		try {
+			Dragonite p = new Dragonite(null,0,null,null,null,0);
+			temp = 0;
+			p1.getBaseExp();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
