eb41a2323a42dabd5777167b13235ce6313d0ac4
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-05-01 18:13:16 +0530

    Update TestMew.java

--- src/testing/TestMew.java
+++ src/testing/TestMew.java
@@ -1,69 +1,70 @@
-//package testing;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.Test;
-//
-//import model.Mew;
-//
-//public class TestMew {
-//	@Test
-//	public void testCharacteristicsMethod() {
-//		Mew p = new Mew("AAA",4);
-//		String temp = "Mew is a Psychic-type Mythical Pok�mon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.";
-//		assertEquals(temp,p.characteristics());		
-//		try {
-//			Mew p1 = new Mew(null,0);
-//			temp = null;
-//			p1.characteristics();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testTypeMethod() {
-//		Mew p = new Mew("AAA",4);
-//		String temp = "PSYCHIC";
-//		assertEquals(temp,p.type());		
-//		try {
-//			Mew p1 = new Mew(null,0);
-//			temp = null;
-//			p1.type();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testEvolutionMethod() {
-//		Mew p = new Mew("AAA",4);
-//		String temp = "   NONE  ";
-//		assertEquals(temp,p.evolution());		
-//		try {
-//			Mew p1 = new Mew(null,0);
-//			temp = null;
-//			p1.evolution();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void testBaseExpMethod() {
-//		Mew p = new Mew("AAA",4);
-//		String temp = "64";
-//		assertEquals(temp,p.baseExp());		
-//		try {
-//			Mew p1 = new Mew(null,0);
-//			temp = null;
-//			p1.baseExp();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package testing;
+/*
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import model.Mew;
+
+public class TestMew {
+	@Test
+	public void testCharacteristicsMethod() {
+		Mew p = new Mew("AAA",4,"Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.","PSYCHIC","   NONE  ",64);
+		String temp = "Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.";
+		assertEquals(temp,p.getCharacteristics());		
+		try {
+			Mew p1 = new Mew(null,0,null,null,null,0);
+			temp = null;
+			p1.getCharacteristics();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testTypeMethod() {
+		Mew p = new Mew("AAA",4,"Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.","PSYCHIC","   NONE  ",64);
+		String temp = "PSYCHIC";
+		assertEquals(temp,p.getType());		
+		try {
+			Mew p1 = new Mew(null,0,null,null,null,0);
+			temp = null;
+			p1.getType();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testEvolutionMethod() {
+		Mew p = new Mew("AAA",4,"Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.","PSYCHIC","   NONE  ",64);
+		String temp = "   NONE  ";
+		assertEquals(temp,p.getEvolution());		
+		try {
+			Mew p1 = new Mew(null,0,null,null,null,0);
+			temp = null;
+			p1.getEvolution();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testBaseExpMethod() {
+		Mew p = new Mew("AAA",4,"Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.","PSYCHIC","   NONE  ",64);
+		int temp = 64;
+		assertEquals(temp,p.getBaseExp());		
+		try {
+			Mew p1 = new Mew(null,0,null,null,null,0);
+			temp = 0;
+			p1.getBaseExp();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
